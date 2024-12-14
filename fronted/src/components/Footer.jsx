942d27bfafa9364813bdc1d8feeeaf8ed3fc9c7b
smitchoksi <smitchoksi219@gmail.com>
2024-12-14 16:14:10 +0530

    first commit

---
+++ fronted/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        {/* left */}
+        <div>
+            <img className='mb-5 w-40' src={assets.logo} alt="" />
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto magni neque molestiae iste eaque aliqui.</p>
+        </div>
+
+         {/* center */}
+         <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Conatct us</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+            {/* right */}
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
+                <li>+91-937-441-8071</li>
+                <li>smitchoksi219@gmail.com</li>
+            </ul>
+        </div>
+      </div>
+      {/* copyright text */}
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024@ Prescripto-All Right Reserved. </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
