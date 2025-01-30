a6b2f2dc39a29071e85ce8f04cbf5469b2382e66
smitchoksi <smitchoksi219@gmail.com>
2025-01-30 13:34:10 +0530

    bhavin2 commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,6 @@ import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import appointmentModel from '../models/appointmentModel.js'
 import Razorpay from 'razorpay';
-import bcrypt from 'bcryptjs'
 
 
 const bcrypt = require('bcryptjs');
